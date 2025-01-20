9d34c7a202eb388c4e7b015336bd238299936afb
AkashKhan0 <beinghuman5320@gmail.com>
2025-01-20 09:46:22 +0600

    port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // console.log("MongoDB URI: ", process.env.MONGODB_URI);
 
